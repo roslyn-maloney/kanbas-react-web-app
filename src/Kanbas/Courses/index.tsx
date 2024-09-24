d07e36aebf23d1de020fbdaa999dc8dc3a3dcbce
roslyn-maloney <roselily1604@gmail.com>
2024-09-24 13:58:48 -0400

    Finished assimnets part of the code

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,7 +1,11 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
+
 import { Navigate, Route, Routes } from "react-router";
+
 export default function Courses() {
   return (
     <div id="wd-courses">
@@ -17,8 +21,8 @@ export default function Courses() {
               <Route path="/" element={<Navigate to="Home" />} />
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<h2>Assignments</h2>} />
-              <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
+              <Route path="Assignments" element={<Assignments />} />
+              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
               <Route path="People" element={<h2>People</h2>} />
             </Routes>
           </td>
