baa403bcbc588325fb22b50b61a6889e6fc56e9a
roslyn-maloney <roselily1604@gmail.com>
2024-11-02 13:14:27 -0400

    Created some of the lab

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,36 @@
+
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <div id="wd-lab4">
+      <h2>Lab 4</h2>
+      <ClickEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable/>
+      {/* <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples /> */}
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
