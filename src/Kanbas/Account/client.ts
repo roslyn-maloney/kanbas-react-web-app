a4d8dc48b1da2e2b32a7f47763ff77122daa37b3
roslyn-maloney <roselily1604@gmail.com>
2024-11-22 20:28:22 -0500

    debugging

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+console.log(REMOTE_SERVER);
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
