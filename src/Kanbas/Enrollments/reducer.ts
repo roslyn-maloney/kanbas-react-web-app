f28da5b4daa5c57e749b19e4916acb0066d116f0
roslyn-maloney <roselily1604@gmail.com>
2024-11-22 20:33:50 -0500

    fixed reducer

--- src/Kanbas/Enrollments/reducer.ts
+++ src/Kanbas/Enrollments/reducer.ts
@@ -1,31 +1,31 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { enrollments } from "../Database";
+import { createSlice } from "@reduxjs/toolkit";
+import { enrollments } from "../Database";
 
-// const initialState = {
-//   enrollments: [],
-// };
-// const enrollmentsSlice = createSlice({
-//   name: "enrollments",
-//   initialState,
-//   reducers: {
-//     setEnrollment: (state, action) => {
-//       state.enrollments = action.payload;
-//     },
-//     addEnrollment: (state, { payload: enrollment }) => {
-//       const newEnrollement: any = {
-//         _id: new Date().getTime().toString(),
-//         name: assignment.name,
-//         course: assignment.course,
-//       };
-//       state.assignments = [...state.assignments, newAssignment] as any;
-//     },
-//     deleteAssignment: (state, { payload: assignmentId }) => {
-//       state.assignments = state.assignments.filter(
-//         (a: any) => a._id !== assignmentId);
-//     },
-//   },
-// });
-// export const { addEnrollment, deleteEnrollment, setEnrollment } =
-//   assignmentsSlice.actions;
+const initialState = {
+  enrollments: [],
+};
+const enrollmentsSlice = createSlice({
+  name: "enrollments",
+  initialState,
+  reducers: {
+    setEnrollment: (state, action) => {
+      state.enrollments = action.payload;
+    },
+    addEnrollment: (state, { payload: enrollment }) => {
+      const newEnrollement: any = {
+        _id: new Date().getTime().toString(),
+        name: enrollment.name,
+        course: enrollment.course,
+      };
+      state.enrollments = [...state.enrollments, newEnrollement] as any;
+    },
+    deleteEnrollment: (state, { payload: enrollId }) => {
+      state.enrollments = state.enrollments.filter(
+        (e: any) => e._id !== enrollId);
+    },
+  },
+});
+export const { addEnrollment, deleteEnrollment, setEnrollment } =
+  enrollmentsSlice.actions;
 
-// export default assignmentsSlice.reducer;
+export default enrollmentsSlice.reducer;
